9ee6e4c1922169494fdf9f6cd1fda6e15e163c35
andrelimaiftm <andrelima@iftm.edu.br>
2022-05-24 20:15:24 -0300

    adicionar codigo referente a aula de hoje

--- src/br/edu/iftm/rpgjava/teste/Principal.java
+++ src/br/edu/iftm/rpgjava/teste/Principal.java
@@ -1,16 +1,20 @@
+package br.edu.iftm.rpgjava.teste;
+
 import java.util.Random;
+
+import br.edu.iftm.rpgjava.classes.Arqueiro;
+import br.edu.iftm.rpgjava.classes.Guerreiro;
+import br.edu.iftm.rpgjava.classes.Mago;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Guerreiro g = new Guerreiro();
-        Mago m = new Mago();
-        Arqueiro a = new Arqueiro();
-
-        //Objeto Guerreiro
-        g.nome = "Vladimir";
-        g.forca = 80;
-        g.agilidade = 30;
+        Guerreiro g = new Guerreiro("Vladimir", 80, 50,30, -3, 0,
+                80, 100, 90, 10, 63); //metodo construtor
+        //Objeto br.edu.iftm.rpgjava.classes.Guerreiro
+        /*g.setNome("Vladimir");
+        g.setForca(80);
+        g.setAgilidade(30);
         g.carisma = 0;
         g.destreza = 50;
         g.inteligencia = -3;
@@ -18,38 +22,44 @@ public class Principal {
         g.resistencia = 80;
         g.sorte = 10;
         g.vida = 100;
-        g.mana = 63;
+        g.mana = 63;*/
 
+
+        Mago m = new Mago("Agostinho Carrara", 0, 90, 0, 95, 0,
+                50, 40, 45, 85, 100);
         //Objeto mago
-        m.nome = "Agostinho Carrara";
+        /*m.nome = "Agostinho Carrara";
         m.mana = 100;
         m.vida = 40;
         m.destreza = 90;
         m.resistencia = 50;
         m.sorte = 85;
         m.inteligencia = 95;
-        m.estamina = 45;
+        m.estamina = 45;*/
 
 
-        //Objeto Arqueiro
-        a.nome = "Jhoson";
+        Arqueiro a = new Arqueiro("Jhoson",0, 90, 0, 60, 0,
+                30, 75, 0, 5, 60);
+        a.setCarisma(20);
+        //Objeto br.edu.iftm.rpgjava.classes.Arqueiro
+        /*a.nome = "Jhoson";
         a.destreza = 90;
         a.resistencia = 30;
         a.estamina = 45;
         a.sorte = 5;
         a.vida = 75;
         a.inteligencia = 60;
-        a.mana = 60;
+        a.mana = 60;*/
 
         //int valorAtaque = g.atacar(6);
 
         Random dado = new Random();
 
         int round  = 1;
-        while(g.vida > 0 && m.vida > 0){
+        while(g.getVida() > 0 && m.getVida() > 0){
             System.out.println("Round: " + round);
-            System.out.println("Oponente 1: " + g.nome);
-            System.out.println("Oponente 2: " + m.nome);
+            System.out.println("Oponente 1: " + g.getNome());
+            System.out.println("Oponente 2: " + m.getNome());
 
             //turno do mago (m)
             int valorDado = dado.nextInt(6);
@@ -65,12 +75,10 @@ public class Principal {
             //round = round +1
             round++;
         }
-        if(m.vida <= 0){
-            System.out.println("Gerreiro " + g.nome + " foi campeão!!!" );
-        }else if(g.vida <= 0){
-            System.out.println("Mago " + m.nome + " foi campeão!!!");
-        }else{
-            System.out.println("Empate!!!");
+        if(m.getVida() <= 0){
+            System.out.println("Gerreiro " + g.getNome() + " foi campeão!!!" );
+        }else if(g.getVida() <= 0){
+            System.out.println("br.edu.iftm.rpgjava.classes.Mago " + m.getNome() + " foi campeão!!!");
         }
 
     }
